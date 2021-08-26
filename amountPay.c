69c66a28c40f4c589a40cc6a86176024508a6aee
Nessvah <vanessamarquesns@outlook.pt>
2021-08-26 14:12:00 +0100

    Finish

--- amountPay.c
+++ amountPay.c
@@ -3,39 +3,67 @@
 int main()
 {
     //Declaring some variables that I might need
-    int payRate = 12;
-    int weekPay, overtimePayrate, overtimePay, hoursWorked, extraHour, grossPay, taxPay, netPay, rate15, rate20, rate25;
+    int overtimePay, hoursWorked, extraHour, grossPay;
+    int payRate = 12; 
+    int overtimePayrate = payRate * 1.5;
 
+    double rate15, rate20, rate25, taxPay, netPay;
 
     printf("Enter number of hours worked in a week:\n");
     scanf("%i", &hoursWorked);
-
-    //Calculate Overtime Pay Rate
-    overtimePayrate = payRate * 1.5; 
-
-    //Calculate normal week pay
-    weekPay = 40 * payRate;
 
     //Calculate overtime
     if (hoursWorked > 40)
     {
        extraHour = hoursWorked - 40;
        overtimePay = extraHour * overtimePayrate;
-       grossPay = weekPay + overtimePay;
+       grossPay = (40 * payRate) + overtimePay;
     }
     else {
-       weekPay = hoursWorked * payRate; 
-       grossPay = weekPay;
+       grossPay = hoursWorked * payRate;
     }
 
     //Calculate Tax Rates
+    if (grossPay > 450)
+    {
+        rate15 = (double)(300 * 15) / 100;
+        rate20 = (double)(150 * 20) / 100;
+        rate25 = (double)(grossPay - 450) * 25 / 100;
 
+        taxPay = rate15 + rate20 + rate25;
+    }
+    else
+    {
+        if (grossPay > 300 && grossPay >= 450)
+        {
+            
+            rate15 = (double)(300 * 15) / 100;
+            rate20 = (double)(grossPay - 300) * 20 / 100;
 
-    
-    
-    
+            taxPay = rate15 + rate20;
+        }
+        
+        else
+        {
+            rate15 = (double)(grossPay * 15) / 100; 
 
+            taxPay = rate15;
+        }
 
-    //Calculate  Net Pay 
-    netPay = grossPay - taxPay;
+    }
+
+    netPay = grossPay - taxPay; 
+
+    printf("Your gross pay this week is %i, the taxes are %.2f and your net pay is %.2f\n", grossPay, taxPay, netPay);
+
+    return 0;
 }
+    
+
+    
+
+    
+    
+    
+
+
